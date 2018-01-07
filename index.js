c5078a09b86300da021b518cb27161807a4952b7
Lee Weisberger <lweisberger@lweisberger-macbookpro.roam.corp.google.com>
2018-01-07 13:42:12 -0800

    updated package structure so now the snake game runs locally

--- index.js
+++ index.js
@@ -2,8 +2,13 @@ const express = require('express');
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+const path = require('path');
 
-app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
+app.use(express.static('client'))
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname + '/client/index.html'));
+});
 
 io.on('connection', (socket) => {
   console.log('a user connected');
