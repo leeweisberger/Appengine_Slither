24d5c6ac9a7e3a8c70196fe0e5c408193b119ba0
Lee Weisberger <lweisberger@lweisberger-macbookpro.roam.corp.google.com>
2018-01-07 13:25:22 -0800

    added client slither game

---
+++ server/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+});
+
+http.listen(3000, () => console.log('Example app listening on port 3000!'));
